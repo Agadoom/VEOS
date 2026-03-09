b144f5cd167ea0e21203bd048716bbb6e0d30e8d
DeepTrade <141333737+Agadoom@users.noreply.github.com>
2026-03-09 02:56:41 +0100

    Use environment variable for bot token

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "YOUR_TOKEN"
+TOKEN = os.getenv("TOKEN")
 
 async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for member in update.message.new_chat_members:
@@ -13,6 +14,7 @@ async def welcome(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 app = ApplicationBuilder().token(TOKEN).build()
+
 app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, welcome))
 
 app.run_polling()
