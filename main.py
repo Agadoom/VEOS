8abe745a8f6b797b4ce709c96bdd80b31f1a1e60
DeepTrade <141333737+Agadoom@users.noreply.github.com>
2026-03-24 21:54:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from telegram import Update, WebAppInfo, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-import config, database, missions, launcher 
+import config, database, missions 
 
 # --- INITIALISATION ---
 try:
