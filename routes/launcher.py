f6f1573f48cdedd4a19126cc805f6c7e8dc924a6
DeepTrade <141333737+Agadoom@users.noreply.github.com>
2026-04-01 13:31:17 +0200

    Update launcher.py

--- routes/launcher.py
+++ routes/launcher.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, Request
+From fastapi import APIRouter, HTTPException, Request
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import database
